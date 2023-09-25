7233fec8d61abdb3f428f6d2c7950ac9c20b5163
nandhakishoresp <122989652+nandhakishoresp@users.noreply.github.com>
2023-09-25 18:46:27 +0530

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,12 @@
+
+var y = math.floor(math.random() *10+1)
+var gess=1
+
+function submit(){
+    document.getElementById("numerInput").value=" "
+}
+
+// count of attempts
+// until hit
+  
+// function for the number sent by the user
